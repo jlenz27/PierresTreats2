bbd94e88e28a9e249f4b2d1ec7f3042c7d090933
John Lenz <john.lenz27@gmail.com>
2023-03-24 12:14:44 -0700

    finished flavor controller

--- PierreTreats/Controllers/TreatsController.cs
+++ PierreTreats/Controllers/TreatsController.cs
@@ -1,21 +1,123 @@
-using Microsoft.AspNetCore.Mvc;
-using PierresTreats.Models;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Mail;
+using System.Reflection.PortableExecutable;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 
-namespace PierresTreats.Controllers;
+using PierresTreats.Models;
 
-[Authorize]
-public class TreatsController : Controller
-{
-  private readonly PierresTreatsContext _db;
-
-  public TreatsController(PierresTreatsContext db)
+namespace RecipeBox.Controllers
+{ 
+  [Authorize]
+  public class TagsController : Controller
   {
-    _db = db;
+    private readonly PierresTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TagsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+  public ActionResult  Create(Treat treat)
+      {
+    
+        _db.Treats.Add(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTag = _db.Treats
+          .Include(fish => fish.JoinEntities)
+          .ThenInclude(fish => fish.Flavor)
+          .FirstOrDefault(tag => tag.TreatId == id);
+      return View(thisTag);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Treat thisTag = _db.Treats.FirstOrDefault(tag => tag.TreatId == id);
+      ViewBag.RecipeId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTag);
+
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat)
+    {
+      _db.Treats.Update(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Treat thisTag = _db.Treats.FirstOrDefault(tag => tag.TreatId == id);
+      return View(thisTag);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Treat thisTag = _db.Treats.FirstOrDefault(tag => tag.TreatId == id);
+      _db.Treats.Remove(thisTag);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddRecipe(int id)
+    {
+      Treat thisTag = _db.Treats
+                          .FirstOrDefault(tags => tags.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTag);
+    }
+
+    [HttpPost]
+    public ActionResult AddRecipe(Treat treat, int flavorId)
+    {
+      #nullable enable
+      FlavorTreat? joinEntity = _db.FlavorTreats.FirstOrDefault(join => (join.FlavorId == flavorId && join.TreatId == treat.TreatId));
+      #nullable disable
+      if (joinEntity == null && flavorId != 0)
+      {
+        _db.FlavorTreats.Add(new FlavorTreat() { FlavorId = flavorId, TreatId = treat.TreatId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = treat.TreatId });
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      FlavorTreat joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreats.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+
   }
 }
---
+++ PierreTreats/Migrations/20230324191259_User.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PierresTreats.Migrations
+{
+    /// <inheritdoc />
+    public partial class User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TreatId",
+                table: "Flavors",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Flavors",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flavors_TreatId",
+                table: "Flavors",
+                column: "TreatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flavors_UserId",
+                table: "Flavors",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flavors_AspNetUsers_UserId",
+                table: "Flavors",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flavors_Treats_TreatId",
+                table: "Flavors",
+                column: "TreatId",
+                principalTable: "Treats",
+                principalColumn: "TreatId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flavors_AspNetUsers_UserId",
+                table: "Flavors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flavors_Treats_TreatId",
+                table: "Flavors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Flavors_TreatId",
+                table: "Flavors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Flavors_UserId",
+                table: "Flavors");
+
+            migrationBuilder.DropColumn(
+                name: "TreatId",
+                table: "Flavors");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Flavors");
+        }
+    }
+}
